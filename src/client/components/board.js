5a8928dc3a56257bbcf060c5a6b1b63938e4f454
emile <guaitanverish2@outlook.com>
2025-03-04 08:27:11 +0100

    problem with calssname of cell

--- src/client/components/board.js
+++ src/client/components/board.js
@@ -1,13 +1,24 @@
-function Board(currentBoard) {
+import { Cell } from './cell';
+import React from 'react';
+
+export function Board({ currentBoard }) {
     return (
         <div className="board">
-        {currentBoard.map((row, rowIndex) => {
-            <div key={rowIndex} className="row">
-                {row.map((cell, cellIndex) => {
-                    <Cell key={rowIndex - cellIndex} className="cell" type={cell}/>
-                })}
-            </div>
-        })}
+            {currentBoard.map((row, rowIndex) => {
+                return (
+                    <div key={rowIndex} className="row">
+                        {row.map((cell, cellIndex) => {
+                            return (
+                                <Cell
+                                    key={`${rowIndex}-${cellIndex}`}
+                                    className="cell"
+                                    Block={cell}
+                                />
+                            )
+                        })}
+                    </div>
+                )
+            })}
         </div>
     );
 }
--- src/client/containers/app.js
+++ src/client/containers/app.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { Board } from '../components/board'
+import { Empty } from '../helper/type'
 
-
-const App = ({message}) => {
+const App = () => {
+  const board = Array(20).fill(null).map(() => Array(12).fill(Empty))
   return (
-    <span>{message}</span>
+    <div className='App'>
+      <Board currentBoard={board}/>
+    </div>
   )
 }
 
@@ -13,6 +17,7 @@ const mapStateToProps = (state) => {
     message: state.message
   }
 }
+
 export default connect(mapStateToProps, null)(App)
 
 
